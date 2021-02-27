5791c99c1e6918c6a289fda673e2a8e4c8eab004
Zlei <zhanglei520sl@126.com>
2021-02-27 13:36:49 +0800

    添加gii脚手架

--- apiadmin/config/main.php
+++ apiadmin/config/main.php
@@ -4,11 +4,24 @@ define('TIME', time());
 return [
     'id' => 'app-apiadmin',
     'defaultRoute'=>'v1/',
+    'bootstrap'=>['gii'],
     'basePath' => dirname(__DIR__),
     'modules' => [ // 添加模块v1和v2，分别表示不同的版本
         'v1' => [
             'class' => 'apiadmin\modules\v1\module'
         ],
+        'gii'=>[
+            'class' => 'yii\gii\Module',
+            'allowedIPs' => ['127.0.0.1'],
+            'generators' => [ 
+                'crud' => [ 
+                    'class' => 'yii\gii\generators\crud\Generator', // generator class
+                    'templates' => [ 
+                        'vishun' => '@backend/views/gii/crud/default', // template name => path to template
+                    ]
+                ]
+            ],
+        ]
     
     ],
     'controllerNamespace' => 'apiadmin\controllers',
