606882516015a5e2935345809f6ff0d9ad92ae5d
Zlei <zhanglei520sl@126.com>
2021-03-25 20:50:32 +0800

    删除无效代码

--- vueadmin/src/page/setting/menu_list_children.vue
+++ vueadmin/src/page/setting/menu_list_children.vue
@@ -133,7 +133,7 @@ export default {
       });
     },
     formatname(row,) {
-      return '|' + row.name;
+      return  row.name;
     },
     formaticon(row, column) {
       return '<i class="' + row.icon + '"></i>';
